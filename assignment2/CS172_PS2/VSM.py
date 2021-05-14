a011c84cc0c10192ffc10de31b3279f2a4e2684a
Eduardo Rocha <eduardor626@yahoo.com>
2021-05-14 11:33:13 -0700

    adding input

--- assignment2/CS172_PS2/VSM.py
+++ assignment2/CS172_PS2/VSM.py
@@ -5,3 +5,23 @@
 # Your program should have TWO command line arguments:
 # path to query file (path to query_list.txt)
 # name of output file / results file 
+
+
+import os
+import sys
+import ast
+from read_index import get_content, get_term_attributes, get_doc_attributes
+
+
+def get_input(arguments):
+    if len(arguments) < 3:
+        print("Error, invalid number of arguments.")
+        return
+    print(arguments)
+
+
+def main():
+    get_input(sys.argv)
+    
+if __name__ == "__main__":
+    main()
