7209092fd5b177cb1aa99f45dfe73a64cafb3cf1
Eduardo Rocha <eduardor626@yahoo.com>
2021-05-13 17:36:58 -0700

    adding file we will be working with

---
+++ assignment2/CS172_PS2/VSM.py
@@ -0,0 +1,7 @@
+#
+# How to run? Lets assume you wrote the code in Python. To run:
+# python VSM.py < query−file > < name−of−results−file >
+# 
+# Your program should have TWO command line arguments:
+# path to query file (path to query_list.txt)
+# name of output file / results file 
