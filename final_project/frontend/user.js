2a3a3a646eefd0589123ed207fe59a0e16a21f28
Eduardo Rocha <eduardor626@yahoo.com>
2021-06-07 15:58:03 -0700

    have tabular formatting the output and source url

--- final_project/frontend/user.js
+++ final_project/frontend/user.js
@@ -9,7 +9,54 @@ function getInputValue() {
         let newDiv = document.getElementById("textbox1");
         console.log(newDiv);
         console.log(data);
-        newDiv.innerHTML = JSON.stringify(data, null, "  ");
+        // newDiv.innerHTML = JSON.stringify(data, null, "  ");
+
+        // EXTRACT VALUE FOR HTML HEADER. 
+        // ('Book ID', 'Book Name', 'Category' and 'Price')
+        var col = [];
+        for (var i = 0; i < data.length + 2; i++) {
+            for (var key in data[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                    console.log(key);
+                }
+            }
+        }
+        console.log(col);
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+
+        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+        var tr = table.insertRow(-1); // TABLE ROW.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th"); // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < data.length; i++) {
+            tr = table.insertRow(-1);
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+
+                if (col[j] == "_source") {
+                    console.log(data[i][col[j]].url)
+                    console.log("url exists!!!");
+                    tabCell.innerHTML = data[i][col[j]].url;
+                } else {
+                    tabCell.innerHTML = data[i][col[j]];
+                }
+            }
+        }
+        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+        newDiv.innerHTML = "";
+        newDiv.appendChild(table);
+
+
     }).catch();
 }
 
